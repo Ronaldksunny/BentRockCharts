26de39744f4fa716cce28564dfde305f28181f5b
Ronald K Sunny <ronaldksunny@gmail.com>
2020-07-20 21:58:05 -0700

    Adding support for xcframework

--- framework/BentrockCharts/build/BentrockCharts.build/Release-iphonesimulator/BentrockCharts.build/DerivedSources/BentrockCharts_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char BentrockChartsVersionString[];
- extern const double BentrockChartsVersionNumber;
-
- const unsigned char BentrockChartsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:BentrockCharts  PROJECT:BentrockCharts-1" "\n";
- const double BentrockChartsVersionNumber __attribute__ ((used)) = (double)1.;
--- framework/BentrockCharts/build/BentrockCharts.build/Release-iphonesimulator/BentrockCharts.build/Objects-normal/x86_64/BentrockCharts_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/a1329183/Desktop/BentrockCharts/build/BentrockCharts.build/Release-iphonesimulator/BentrockCharts.build/DerivedSources/BentrockCharts_vers.c
